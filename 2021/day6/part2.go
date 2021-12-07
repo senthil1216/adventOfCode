76e0ecdfe31e879c285ecd5ade756fa408b6f0c3
senthil sivasubramanian <senthil1216@gmail.com>
2021-12-07 09:51:43 -0800

    solved day6

---
+++ 2021/day6/part2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//file, _ := os.Open("./input_back")
+	file, _ := os.Open("./input")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var nums []int
+	var m = make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines := strings.Split(line, ",")
+		for _, l := range lines {
+			num, _ := strconv.Atoi(l)
+			nums = append(nums, num)
+		}
+	}
+	m[1000] = 100
+	//var i int
+	// for i < 80 {
+	// 	var count int
+	// 	for idx, num := range nums {
+	// 		if num == 0 {
+	// 			nums[idx] = 6
+	// 			count++
+	// 		} else {
+	// 			nums[idx]--
+	// 		}
+	// 	}
+	// 	for c := 0; c < count; c++ {
+	// 		nums = append(nums, 8)
+	// 	}
+	// 	i++
+	// }
+	fmt.Println(len(nums))
+}
